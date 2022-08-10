3c27d9db1b4c7a525721f389a670d0d27c8c5307
gpelayo <gpelayo109@gmail.com>
2022-08-09 19:45:46 -0700

    Fixed analysis fields typos

--- positivitytoart-articlepostscraper/chalicelib/analysis.py
+++ positivitytoart-articlepostscraper/chalicelib/analysis.py
@@ -25,16 +25,16 @@ class NewsReader:
                 scrapped_article = NewsPlease.from_url(db_post.url)
             except LxmlError as e:
                 db_article = ArticleAnalysis(db_post.article_id, db_post.title, db_post.url)
-                db_article.analyze_status = 'ERROR'
+                db_article.analysis_status = 'ERROR'
                 error_log = e.error_log if hasattr(e, "error_log") else ""
-                db_article.analyze_comments = f'({type(e).__name__}){e} {error_log}'
+                db_article.analysis_comments = f'({type(e).__name__}){e} {error_log}'
             else:
                 logging.log(logging.INFO, f'Read and wrote {scrapped_article.title}')
                 db_article = ArticleAnalysis(db_post.article_id, scrapped_article.title, db_post.url)
                 db_article.date_written = scrapped_article.date_publish
                 db_article.description = scrapped_article.description
                 db_article.main_text = scrapped_article.maintext
-                db_article.analyze_status = 'OK'
+                db_article.analysis_status = 'OK'
 
             db_articles.append(db_article)
 
