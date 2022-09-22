2efc94fe2c78eb50042e67b849fe26d786723212
gpelayo <gpelayo109@gmail.com>
2022-09-21 20:09:30 -0700

    Added hashtag generator

--- positivitytoart-artposterapi/app.py
+++ positivitytoart-artposterapi/app.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-import json
 from urllib.parse import urlparse
 
+import boto3
 from chalice import Chalice, BadRequestError
+import requests
 
 from chalicelib.database import Database
-from urllib.parse import urlparse
-from datetime import datetime
+from chalicelib.settings import hashtag_queue, queue_url
 
 app = Chalice(app_name='positivitytoart-artposterapi')
 
@@ -17,6 +17,37 @@ def about():
         'message': 'Positivity to Art is a project for delivering positive news with AI generated art pieces.'
                    ' This API contains collection of NLP and Image Generation tools for Positivity to Art.'
     }
+
+
+max_hashtag_attempts = 3
+
+
+@app.on_sqs_message(queue=hashtag_queue)
+def generate_hashtags(event):
+    with Database() as database:
+        for record in event:
+            article_id = record.body
+            article_analysis = database.get_article_analysis(article_id)
+            complete_text = f"{article_analysis.main_text} {article_analysis.description}"
+            request_json = {
+                'text': complete_text
+            }
+            attempts = 0
+            while max_hashtag_attempts < attempts:
+                attempts = 0
+                hashtag_response = requests.post('https://1x150zj9w5.execute-api.us-west-2.amazonaws.com/api/v1/hashtag',
+                                                 json=request_json)
+                if hashtag_response.ok:
+                    break
+            hashtags = hashtag_response.json().get('hashtags')
+            database.write_suggested_hashtags(article_analysis.article_id, hashtags)
+
+
+@app.route('/v1/budibase/hashtags/{article_id}', methods=['POST'])
+def send_hashtag_request(article_id):
+    sqs = boto3.client('sqs')
+    sqs.send_message(QueueUrl=queue_url, MessageBody=article_id)
+    return {'message': f'Starting process to generate hashtags for article, {article_id}'}
 
 
 @app.route('/v1/budibase/post_schedule', methods=['POST'])
--- positivitytoart-artposterapi/chalicelib/settings.py
+++ positivitytoart-artposterapi/chalicelib/settings.py
@@ -1,5 +1,7 @@
 import os
 
+aws_region = os.environ.get('AWS_REGION')
+aws_user = os.environ.get('AWS_USER')
 rdb_host = os.environ.get('RDB_HOST')
 rdb_port = os.environ.get('RDB_PORT')
 rdb_user = os.environ.get('RDB_USER')
@@ -8,3 +10,5 @@ rdb_database_name = os.environ.get('RDB_DATABASE_NAME')
 openai_client = os.environ.get('OPENAI_ORG')
 openai_password = os.environ.get('OPENAI_APIKEY')
 openai_config_bucket_name = os.environ.get('OPENAI_CONFIG_S3_BUCKET_NAME')
+hashtag_queue = os.environ.get('HASHTAG_QUEUE')
+queue_url = f'https://sqs.{aws_region}.amazonaws.com/{aws_user}/{hashtag_queue}'
